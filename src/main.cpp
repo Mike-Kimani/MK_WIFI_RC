2cba172c7a4fd8d36237f97ca97c42d83567b327
Mike-Kimani <mikalkimmani@gmail.com>
2022-05-22 12:27:50 +0300

    Indicators

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include <functions.h>
+#include <defs.h>
+
+//pinMode(2, OUTPUT);
 
 void setup() {
   // put your setup code here, to run once:
+pinMode(blueonboard, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
